f02a67c75371f6128a270b53ab824f5cdc607af5
Ahmed-Tombari <108124844+Ahmed-Tombari@users.noreply.github.com>
2022-07-12 10:01:33 +0100

    Add files via upload

---
+++ CrudApplication-Backend/src/main/java/com/crudapplication/repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.crudapplication.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+import com.crudapplication.model.Employee;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
